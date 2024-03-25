899fd304ae4ba35daa474d533e2fba9a31706e26
ChronoByte404 <jackfranklin899@gmail.com>
2024-03-25 11:45:22 +0000

    Automation

--- Internal/functionality/core.cpp
+++ Internal/functionality/core.cpp
@@ -2,6 +2,11 @@
 #include <cstdlib>
 #include <thread>
 #include <chrono>
+#include <cstdio>
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int printer(const std::string& templateFile) {
     std::string command = "python3 ./Internal/printing/printer.py " + templateFile;
@@ -29,9 +34,32 @@ int game() {
     return 0;
 }
 
+std::string menu_interface() {
+    char option[256];
+    printf("\nWhich eXit game would you like to play? [DoctorWho, MrRobot]\n\nGame: ");
+    scanf("%s", option);
+    return std::string(option);
+}
+
+int start_game(const std::string& option) {
+    std::string path = "./Games/" + option + "/";
+    // Check if the directory exists
+    if (fs::is_directory(path)) {
+        // Directory exists, do something
+        std::cout << "Directory exists: " << path << std::endl;
+        // You can list files in the directory or perform other operations here
+    } else {
+        std::cerr << "Directory does not exist: " << path << std::endl;
+    }
+    return 0;
+}
+
 int main() {
     int phil, sg;
+    std::string option;
     phil = boot();
     sg = game();
+    option = menu_interface();
+    start_game(option);
     return 0;
 }
